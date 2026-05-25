beb4fe36e412315244dfd1afefa1cc9b81ae9d6d
K Huehn <slowebworkz@gmail.com>
2026-05-25 16:06:18 -0700

    chore: replace URL alias blocks with resolve.tsconfigPaths on configs that have tsconfig paths

--- packages/docs/vitest.config.ts
+++ packages/docs/vitest.config.ts
@@ -2,20 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   resolve: {
-    alias: {
-      '@polymorphic-ui/core': new URL('../core/src/index.ts', import.meta.url).pathname,
-      '@polymorphic-ui/primitive': new URL('../../lib/primitive/src', import.meta.url).pathname,
-      '@polymorphic-ui/contract': new URL('../../lib/contract/src', import.meta.url).pathname,
-      '@polymorphic-ui/styling': new URL('../../lib/styling/src', import.meta.url).pathname,
-      '@polymorphic-ui/adapter-utils': new URL('../../lib/adapter-utils/src', import.meta.url)
-        .pathname,
-      '@polymorphic-ui/react': new URL('../react/src/index.ts', import.meta.url).pathname,
-      '@polymorphic-ui/tailwind': new URL('../tailwind/src/index.ts', import.meta.url).pathname,
-      '@polymorphic-ui/vue': new URL('../vue/src/index.ts', import.meta.url).pathname,
-      '@/shared': new URL('../react/src/shared', import.meta.url).pathname,
-      '@/current': new URL('../react/src/current', import.meta.url).pathname,
-      '@/legacy': new URL('../react/src/legacy', import.meta.url).pathname,
-    },
+    tsconfigPaths: true,
   },
   test: {
     name: 'docs',
