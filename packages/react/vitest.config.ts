80cb303340c1c684406062d93348ce802cb9bcc8
K Huehn <slowebworkz@gmail.com>
2026-05-25 16:06:18 -0700

    chore: replace URL alias blocks with resolve.tsconfigPaths on configs that have tsconfig paths

--- packages/react/vitest.config.ts
+++ packages/react/vitest.config.ts
@@ -2,17 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   resolve: {
-    alias: {
-      '@polymorphic-ui/core': new URL('../core/src/index.ts', import.meta.url).pathname,
-      '@polymorphic-ui/primitive': new URL('../../lib/primitive/src', import.meta.url).pathname,
-      '@polymorphic-ui/contract': new URL('../../lib/contract/src', import.meta.url).pathname,
-      '@polymorphic-ui/styling': new URL('../../lib/styling/src', import.meta.url).pathname,
-      '@polymorphic-ui/adapter-utils': new URL('../../lib/adapter-utils/src', import.meta.url)
-        .pathname,
-      '@/shared': new URL('./src/shared', import.meta.url).pathname,
-      '@/current': new URL('./src/current', import.meta.url).pathname,
-      '@/legacy': new URL('./src/legacy', import.meta.url).pathname,
-    },
+    tsconfigPaths: true,
   },
   test: {
     name: 'react',
