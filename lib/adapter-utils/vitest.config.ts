14d571060975242bfd47b14e8a90ca806b9549ef
K Huehn <slowebworkz@gmail.com>
2026-05-25 16:06:18 -0700

    chore: replace URL alias blocks with resolve.tsconfigPaths on configs that have tsconfig paths

--- lib/adapter-utils/vitest.config.ts
+++ lib/adapter-utils/vitest.config.ts
@@ -2,12 +2,7 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   resolve: {
-    alias: {
-      '@polymorphic-ui/core': new URL('../../packages/core/src/index.ts', import.meta.url).pathname,
-      '@polymorphic-ui/primitive': new URL('../primitive/src', import.meta.url).pathname,
-      '@polymorphic-ui/contract': new URL('../contract/src', import.meta.url).pathname,
-      '@polymorphic-ui/styling': new URL('../styling/src', import.meta.url).pathname,
-    },
+    tsconfigPaths: true,
   },
   test: {
     name: 'adapter-utils',
