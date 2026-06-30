50322b6149f4f12600c5e96916a6201078c121e0
K Huehn <slowebworkz@gmail.com>
2026-06-29 23:18:16 -0700

    chore(pk2): remove REMOVE-NOW items from architecture audit
    - Drop deprecated StrictBase alias from invariant-base.ts
    - Delete empty packages/shared/src/types/config/ barrel (orphaned after StrictMode removal)
    - Remove createPolymorphicComponent from vite-plugin DEFAULT_CALLEE_NAMES (pre-rename name)

--- packages/vite-plugin/src/constants.ts
+++ packages/vite-plugin/src/constants.ts
@@ -1,3 +1,3 @@
-export const DEFAULT_CALLEE_NAMES = ['createPolymorphicComponent', 'createContractComponent']
+export const DEFAULT_CALLEE_NAMES = ['createContractComponent']
 export const JSX_EXTS = new Set(['tsx', 'jsx'])
 export const ALL_EXTS = new Set(['ts', 'tsx', 'js', 'jsx'])
